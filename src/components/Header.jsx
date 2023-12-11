622eaa05b3f736e0ba7773c5f92909fedc8eecda
Ammarkhan96 <ammarkhaan96@gmail.com>
2023-12-11 20:14:51 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+const Header = () => {
+          const [t, i18n] = useTranslation("global");
+
+          const handleChangeLanguage = (lang) =>{
+                    i18n.changeLanguage(lang)
+
+          }
+   return (
+    <div>
+      <h1>{t("header.message")}</h1>
+      <button onClick={() => handleChangeLanguage("en")}>ENGLISH</button>
+      <button onClick={() => handleChangeLanguage("es")}>SPANISH</button>
+    </div>
+  )
+}
+
+export default Header
